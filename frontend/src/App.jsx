b20d93d4bce5553d4324b274d329ff3b916122a5
kshitijgetsac <kshitijtaneja2000@gmail.com>
2024-09-03 10:59:49 -0400

    signin component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
-import { SignUp } from "./components/Signup.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SignIn } from "./components/SignIn.jsx";
+import { SignUp } from "./components/SignUp.jsx";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <SignUp></SignUp>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing></Landing>}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
+function Landing() {
+  return <div>Welcome to Paytm</div>;
+}
 export default App;
