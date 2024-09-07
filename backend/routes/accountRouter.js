e0eddc8a14e3052538d6bef8f1d13bc3cacc31f4
kshitijgetsac <kshitijtaneja2000@gmail.com>
2024-09-07 17:06:36 -0400

    balance and transfer endpoints added

--- backend/routes/accountRouter.js
+++ backend/routes/accountRouter.js
@@ -19,5 +19,57 @@ accountRouter.get("/balance", authMiddleware, async (req, res) => {
   console.log(AccountDetails.balance);
   res.status(200).json({ balance: AccountDetails.balance });
 });
+const TransferBody = z.object({
+  ToUserId: z.string(),
+  balance: z.number().int(),
+});
+accountRouter.post("/transfer", authMiddleware, async (req, res) => {
+  const success = TransferBody.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      msg: "invalid inputs",
+    });
+    return;
+  }
+  const transferrerDetails = await Account.findOne({
+    userid: req.userid,
+  });
+  if (transferrerDetails.balance < req.body.balance) {
+    res.status(415).json({
+      msg: "insufficient balance",
+    });
+    return;
+  }
+
+  //do the rest of work using transactions
+  //to maintain ACID properties
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const transferee = await Account.findOneAndUpdate(
+      {
+        userid: req.body.touserid,
+      },
+      { $inc: { balance: req.body.balance } }
+    );
+    const Transferrer = await Account.findOneAndUpdate(
+      {
+        userid: req.userid,
+      },
+      { $inc: { balance: -1 * req.body.balance } }
+    );
+    await session.commitTransaction();
+    session.endSession();
+    res.status(200).json({
+      msg: "money sent successfully",
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    res.status(415).json({
+      msg: "something went wrong",
+    });
+  }
+});
 
 module.exports = accountRouter;
